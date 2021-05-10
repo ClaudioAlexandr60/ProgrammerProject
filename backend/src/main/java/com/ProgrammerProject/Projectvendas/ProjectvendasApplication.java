5dd44246dc00cf6001bac94d6b159901c50ec2c6
Claudioalexandr60 <mrleonbr50@gmail.com>
2021-05-09 21:35:30 -0300

    Project Created

---
+++ backend/src/main/java/com/ProgrammerProject/Projectvendas/ProjectvendasApplication.java
@@ -0,0 +1,13 @@
+package com.ProgrammerProject.Projectvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ProgrammerProject/Projectvendas/ProjectvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProgrammerProject.Projectvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
